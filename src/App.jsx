b3e7b54499920603b2046b8b12815d3e83f57d72
Shiva verma <shivaverma@affordmed.com>
2026-07-08 07:46:00 +0530

    Add query search

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Card from "./components/Card";
 import { cardsData } from "./data";
 
 const App = () => {
   const [search, setSearch] = useState("");
   const [data, setData] = useState(cardsData);
-  const timer = useRef(null)
+  const timer = useRef(null);
+
+  const params = new URLSearchParams(window.location.search);
+
   const handleSearch = (e) => {
-    let val = e.target.value
+    let val = e.target.value.trim();
     setSearch(val);
-    clearInterval(timer.current)
-    timer.current = setTimeout(()=>filterCard(val),300)
+    if (val !== "") params.set("search", val);
+    else params.delete("search");
+    window.history.replaceState(
+      {},
+      "",
+      `${window.location.pathname}?${params.toString()}`,
+    );
+
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => filterCard(val), 300);
   };
 
   const filterCard = (val) => {
-    val = val.toLowerCase().trim();
+    val = val.toLowerCase();
     const filteredData = cardsData.filter(
       (card) =>
         card.title.toLowerCase().includes(val) ||
@@ -24,7 +35,13 @@ const App = () => {
     setData(filteredData);
   };
 
-  
+  useEffect(() => {
+    const value = params.get("search");
+    if (value) {
+      filterCard(value);
+      setSearch(value);
+    }
+  }, []);
   return (
     <div className="app-wrapper roboto">
       <h1>Product Card Filter</h1>
